58bb2306b7deda47864986b699c42a2431dd0f5b
SefBoonen <1boonen.sef1@gmail.com>
2025-10-03 15:38:28 +0200

    Changed to module

--- web/backend/index.mjs
+++ web/backend/index.mjs
@@ -1,11 +1,11 @@
-const path = require("path");
-const express = require("express");
+import path from "path";
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "..", "frontend")));
+app.use(express.static(path.join("..", "frontend")));
 
 app.listen(3000, "127.0.0.1", () => {
     console.log(`Server running at http://127.0.0.1:3000`);
