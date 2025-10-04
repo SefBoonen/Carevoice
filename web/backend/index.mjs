987d55db7824d598179809901cafc740a857b008
SefBoonen <1boonen.sef1@gmail.com>
2025-10-04 19:29:47 +0200

    Added websocket backedn code

--- web/backend/index.mjs
+++ web/backend/index.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import * as path from "path";
+import { WebSocketServer } from "ws";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,4 +14,20 @@ app.use(express.static(path.join(__dirname, "..", "frontend")));
 
 app.listen(3000, () => {
     console.log(`Server running at http://localhost:3000`);
+});
+
+const wss = new WebSocketServer({ server, path: "/audio" });
+
+wss.on("connection", (ws) => {
+    console.log("Client connected");
+    const fileStream = fs.createWriteStream(`recording_${Date.now()}.webm`)
+
+    ws.on("message", (data) => {
+        fileStream.write(data);
+    })
+
+    ws.on("close", () => {
+        fileStream.end();
+        console.log("audio saved")
+    })
 });
